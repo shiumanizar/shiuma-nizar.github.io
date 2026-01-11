08621a20824d521abc51ea17f4f7048f1efd8bab
shiumanizar <shiuma.nizar@gmail.com>
2026-01-11 10:52:30 +0500

    changing hero image

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import HeroPic from "./assets/hero-image.jpg";
+import HeroPic from "./assets/hero-image-v2.jpg";
 import { FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
